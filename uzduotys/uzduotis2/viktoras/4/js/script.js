8e8497b4b0468b318050eb71383181b7ca4ca5c7
viktorasj <viktoras.jefimovas@gmail.com>
2017-11-15 00:51:33 +0200

    12:51AM

--- uzduotys/uzduotis2/viktoras/4/js/script.js
+++ uzduotys/uzduotis2/viktoras/4/js/script.js
@@ -1,8 +1,35 @@
+function makeMenu (){
+  rect.style.width = " 12em";
+  rect.style.height = " 12em";
+  rectBack.style.opacity = " 1";
+  firstMenu.style.opacity = " 1";
+  secondMenu.style.opacity = " 1";
+  thirdMenu.style.opacity = " 1";
+  fourtMenu.style.opacity = " 1";
+  fifthMenu.style.opacity = " 1";
+}
+
+function goBack () {
+  rect.style.width = " 10em";
+  rect.style.height = " 10em";
+  rectBack.style.opacity = " 0";
+  firstMenu.style.opacity = " 0";
+  secondMenu.style.opacity = " 0";
+  thirdMenu.style.opacity = " 0";
+  fourtMenu.style.opacity = " 0";
+  fifthMenu.style.opacity = " 0";
+
+}
 
 window.onload = function () {
+
 var rect = document.getElementById('rect');
-rect.onclick = function () {
-  rect.classList.add("stayBigger");
-  rect.classList.remove("beBigger");
-};
+var rectBack = document.getElementById('rectBack');
+var firstMenu = document.getElementById('firstMenu');
+var secondMenu = document.getElementById('secondMenu');
+var thirdMenu = document.getElementById('thirdMenu');
+var fourtMenu = document.getElementById('fourtMenu');
+var fifthMenu = document.getElementById('fifthMenu');
+rect.onclick = makeMenu;
+rectBack.onclick = goBack;
 };
