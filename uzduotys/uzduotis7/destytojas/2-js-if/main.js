bc0557815b842ba6c916d810431072758a96b88f
grajauskas <e.grajauskas@gmail.com>
2017-11-21 11:20:02 +0200

    aaa

---
+++ uzduotys/uzduotis7/destytojas/2-js-if/main.js
@@ -0,0 +1,21 @@
+console.log("lasbas");
+
+
+var age = 24;
+
+iki 7 metu
+"Pliusines varles"
+
+nuo 7 iki 14
+"Mini telefonai"
+
+nuo 14 iki 18
+"new Music App"
+
+nuo 18 iki 24
+"Stok i sauliu sajunga"
+
+nuo 24 iki 65
+"Pensijos kaupimas - zusiregistruok"
+nuo 65
+"kelione i Bristona"
