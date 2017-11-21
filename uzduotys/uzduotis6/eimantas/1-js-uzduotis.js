3562462f98eb7c0ac374e4d7c426d4f4e81531ed
gustas93 <augustasbu@gmail.com>
2017-11-21 11:46:39 +0200

    javascript
    js uzduotys

--- uzduotys/uzduotis6/eimantas/1-js-uzduotis.js
+++
@@ -1,16 +0,0 @@
-
-
-//  1 UZDUOTIS// sukurti kintamuosius : vardas, pavarde, amzius, atlyginimas
-
-
-//  2 UZDUOTIS// sukurti funkcija "printVardas()"
-
-
-
-//  3 UZDUOTIS// sukurti funkcija "printMetinisPajamuDydis()"
-
-
-//  4 UZDUOTIS// sukurti kintamuosius:  salis, miestas, adresas, pastoKodas// ir juos isvesti/atspausdinti i konsole
-
-
-//  5 UZDUOTIS// sukurti funkcija "printTekstas(x)"
