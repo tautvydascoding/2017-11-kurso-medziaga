0e97418c55b1113d4d50b03c881fd3accf6063f6
KCSeimantas <eimutisuk@live.com>
2017-11-21 17:02:16 +0200

    js uzduotys

---
+++ uzduotys/uzduotis7/eimantas/jsTest.js
@@ -0,0 +1,6 @@
+console.log("Hi from JS");
+
+for (var i = 0; i < 50; i++) {
+    document.getElementById("article").innerHTML = "<h2> Azuolas</h2><br>";
+    console.log("kasdhk");
+}
--- uzduotys/uzduotis7/eimantas/jsUzduotys.js
+++ uzduotys/uzduotis7/eimantas/jsUzduotys.js
@@ -12,9 +12,9 @@ console.log ("Labas");
 // sukurti 3 Globalius kintamuosius vardas , pavarde, klase
 var vardas = "vardux";
 var pavarde = "pavardux";
-var klase = "klase?";
+var klase = "klase";
 
-// 1B
+//
 // atspausdinti visus kintamuosius
 
 console.log(vardas, pavarde, klase);
@@ -35,33 +35,83 @@ function printClass(){
 
 // 3
 // parasyti f-ja printNameLNameClass(x, y, z)
-//
+
+
+function printNameLNameClass(x, y, z){
+    console.log("vardas: ", x, "pavarde: ", y, "klase: ", z);
+}
+
+printNameLNameClass("tomas", "tomauskas", "5");
+printNameLNameClass("darius", "noobas", "2");
+printNameLNameClass("Egis", "siaip", "7");
 
 // 4
 // parasyti f-ja spausdintiPazymius(x, y, z)
-//
+
+function spausdintiPazymius(x, y, z){
+    console.log("Rugsejis: ", x);
+    console.log("Spalis: ", y);
+    console.log("Lapkritis: ", z);
+}
+
+spausdintiPazymius(1, 2, 6);
+
  // 4.1
 //  atspausdinti 4 vaiku pazymius
-//
+printNameLNameClass("tomas", "tomauskas", "5");
+spausdintiPazymius(1, 2, 6);
+
+printNameLNameClass("kitas", "tomauskas", "5");
+spausdintiPazymius(1, 2, 6);
+
+printNameLNameClass("dar", "tomauskas", "5");
+spausdintiPazymius(1, 2, 6);
 
 // 5
 // issokanciam lange paprasyti ivesti vartotoja savo amziu
+var vAmzius = prompt("Iveskite savo amziu");
+
 // 5.1
 // atspaudinti vartotojo amziu i konsole
 
-
+console.log("ivestas amzius", vAmzius);
 
 // ======FOR   LOOP=======
  // 1.
  // isvesti 50 kartu zodi "Azuolas" i konsole
 
+ // for (var i = 0; i < 50; i++) {
+ //     console.log("Azuolas" + (i+1));
+ // }
+
  // 2.1
  //
  // isvesti 50 kartu zodi "<h2> Azuolas</h2> " i <article> elementa su  INNERHTML pagalba
 
+document.querySelector("article").innerHTML = "";
+
+for (var i = 0; i < 5; i++) {
+    document.querySelector("article").innerHTML += "<h2>Azuolas</h2><br>";
+    document.querySelector("article").innerHTML += "<p>ahsflahsdlaksj;fajsk </p>";
+}
 
 // 2 A
 // sukurti f-ja printX(xx) i browser langa
+
+function printX(xx){
+    document.querySelector("article").innerHTML += xx;
+}
+
+var txt = "<h1>kazkas per info</h1>";
+printX(txt);
+
+// spausdina X, Y vietoje - vienkartiniai parametrai
+function printXY(x, y){
+    document.querySelector(y).innerHTML += x;
+}
+
+printXY("laba dena", "h3");
+
 // 2B
 // paleisti f-ja printX 100 kartu
 
