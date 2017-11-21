46bc4bf0ba0b3a4fb862b8759ebd0df2586968f6
grajauskas <e.grajauskas@gmail.com>
2017-11-21 11:25:09 +0200

    aaa

--- uzduotys/uzduotis7/destytojas/2-js-if/main.js
+++ uzduotys/uzduotis7/destytojas/2-js-if/main.js
@@ -19,3 +19,9 @@ nuo 24 iki 65
 "Pensijos kaupimas - zusiregistruok"
 nuo 65
 "kelione i Bristona"
+
+// 2 uzduotis
+// sukurti 3 vardus "Tomas", "Paulius", "Airidas"
+// kai "if"-ui padauodame Toma - isvesti "Masinoms 10% nuolaida"
+// kai "if"-ui padauodame Pauliu - isvesti "Buitinei technikai  30% nuolaida"
+// kai "if"-ui padauodame bet ka kita - isvesti "5% nuolaida kelionems"
