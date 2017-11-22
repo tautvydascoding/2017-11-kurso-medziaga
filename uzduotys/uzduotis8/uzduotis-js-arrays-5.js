bcf6a3e727d199bc77e73ad12a73a3b494bf5351
Tautvydas <arttgd@gmail.com>
2017-11-22 08:37:11 +0200

    aa

---
+++ uzduotys/uzduotis8/uzduotis-js-arrays-5.js
@@ -0,0 +1,15 @@
+// apsilimui
+// 1 kursime nauja masyva uzpildyta, atsitiktiniais skaiciais nuo 10 - 100
+// 2 uzdavinys: pakeisti atsitiktinius skaicius, kurie yra lyginiai i zodi "lyginis"
+
+
+// rimta
+// 1 kursime nauja masyva uzpildyta, atsitiktiniais skaiciais nuo 50 - 200 (pagamintomis datalemis per diena)
+ 
+// 2 burtu keliu 10 % tikimybe paversti minus reiksememis ( padauginti is  -1)
+// 3. uzdavinys: visus skaicius paversti teigiamas (is masyvo parts)
+// 4. surasti geriausia darbuotoja
+// 5. surasti blogiausia darbuotoja
+  
+ 
+ 
