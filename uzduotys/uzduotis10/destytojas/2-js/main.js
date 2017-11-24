603b5e06167fb091dca0f5d12a077d6e5bc666cc
grajauskas <e.grajauskas@gmail.com>
2017-11-24 11:10:47 +0200

    10 diena darbas 1

---
+++ uzduotys/uzduotis10/destytojas/2-js/main.js
@@ -0,0 +1,39 @@
+console.log("lasbas");
+
+// ----------------------Matrix---------------------------
+var data = [];
+var zmogus1 = ["Tomas", "Tomulis", 1998];
+var zmogus2 = ["Birute", "Bitutaite", 1966];
+var zmogus3 = ["Olga", "Opapa", 1985];
+data[0] = zmogus1;
+
+console.log( data );
+console.log( data[0] );
+
+data[1] = zmogus2;
+data[2] = zmogus3;
+
+console.log(data);
+document.querySelector('article').innerHTML += "....";
+
+
+// 1 uzduotis
+// isvesti visus duomenis apie zmogu vienoje eilute
+
+// 2 Uzduotis
+// isvesti visus duomenis stulpeliu
+
+// 3 uzduotis
+// pakeisti "Olgos " pavarde i "Jarukuotiete"
+
+// 4 Uzduotis
+// automatiskai patikrinti visus duomenis ir visus gimusius nuo 1980 iki 2000 isvesti
+
+
+
+
+
+
+
+
+//
