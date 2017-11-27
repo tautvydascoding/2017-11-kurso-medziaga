b462f100aa9eee2f583fa1a92655d04282ba7213
KCSeimantas <eimutisuk@live.com>
2017-11-27 14:06:36 +0200

    pakitimai

--- uzduotys/uzduotis10/eimantas/jQuery/main.js
+++ uzduotys/uzduotis10/eimantas/jQuery/main.js
@@ -1,1 +1,62 @@
-console.log("lasbas"); 
+console.log("hi.");
+
+// Kintamieji
+var antraste = $("h1, h3");
+// CSS su objektu
+var stilius = {
+    color: "orange",
+    "font-size": "80px",
+    "background-color": "pink",
+    "font-weight": 100
+};
+var backIrFontON = {
+    "background": "yellow",
+    "font-size": 24
+};
+var backIrFontOUT = {
+    "background": "none",
+    "font-size": 20
+};
+
+
+antraste.css("color", "blue");
+
+
+antraste.css(stilius);
+
+// jei vienodi elementai, priskirs tik paskuti nurodyma
+$("h2").css("color", "red").add("h3").css("font-size", 14).add("h2").css("color", "black");
+
+
+// h2 ieskos main elemente
+$("h2", "main").css("background", "red");
+
+
+$("li:even").css("color", "green");
+$("li:odd").css("color", "red");
+
+$( "li" ).mouseover(function() {
+    $(this).css(backIrFontON);
+});
+$( "li" ).mouseout(function() {
+    $(this).css(backIrFontOUT);
+});
+
+// suranta h2 main elemente
+$("main").find("h2").css("font-size", 65);
+
+
+$('input[type="email"]').css("background-color", "#b2b2b2");
+// issaugo vartotojo ivesta reiksme
+var a = $('input[type="email"]').val();
+
+
+$("li").siblings().css('property', 'value');
+
+// .next() sekantis brolinis elementas
+
+// .nextAll() viskas kas po juo
+
+// .nextUntil() viska po iki nurodyto
+
+//
---
+++ uzduotys/uzduotis10/eimantas/praktika-mas-mat/main.js
@@ -0,0 +1,28 @@
+
+var randNumers = [];
+var matricosMas = [];
+var data = [1665, 2665, 3665, 4665, 5665];
+var min = 1;
+var max = 100;
+
+
+// sukuria masyva su 10 skirtingu skaiciu
+// for (var i = 0; i < 10; i++) {
+//     randNumers[i] = Math.floor(Math.random() * max - min) + min;
+//
+//
+// }
+// console.log(randNumers);
+
+// sukuria matrica
+for (var k = 0; k < data.length; k++) {
+    // i tuscio masyvo kiekviena elementa idedu masyva
+    matricosMas[k] = data;
+}
+console.log(matricosMas);
+
+
+
+
+
+//
