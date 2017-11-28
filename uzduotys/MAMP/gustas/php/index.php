4050166226df9f69ccecef07d6ccce0b5c7844e4
Gustas <augustasbu@gmail.com>
2017-11-28 16:46:01 +0200

    antradienio vakaro
    komitas

--- uzduotys/MAMP/gustas/php/index.php
+++ uzduotys/MAMP/gustas/php/index.php
@@ -29,3 +29,53 @@
 
     </body>
 </html>
+
+
+
+<?php
+//komentaras
+/* kitoks komentaras
+kuri galima rasyti per kelias
+eilutes*/
+
+
+
+echo "siandien mokomes php </br>";
+$name = "Tomas";
+$age = 21.00000;
+  echo "realus age: $age <br />";
+  // gettype($age);
+  settype($age, 'integer');
+
+
+// + nejungia string , bet . nejungia
+echo "labas $name </br>";
+echo "labas ".$name."</br>";
+
+define("DB_NAME", "ryklys");
+define("DB_PASSWORD", "kazinkas");
+define("HOST", "localhost");
+define("USERNAME", "root");
+
+echo "duomenu bazes pavadinimas " . DB_NAME."</br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
