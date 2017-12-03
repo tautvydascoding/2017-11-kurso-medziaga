b1a5b27e6186be1e838565f7c662097642a05cdc
Linassil <33597256+Linassil@users.noreply.github.com>
2017-12-03 18:10:21 +0200

    1414

--- uzduotys/MAMP/uzduotis15/linas/db.sql
+++ uzduotys/MAMP/uzduotis15/linas/db.sql
@@ -1,9 +1,64 @@
-﻿mysql>
+mysql>
 
 show databases;
 use duomenuBazesPavadinimas;
 show tables;
 
+
+-- // automatiskai dides nuo 1 iki begalybes kai idesime duomenis
+-- // PRIMARY KEY - raktinis stulpelis, !!! jame reiksmes negali kartotis
+
+CREATE TABLE doctors (
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25),
+    lname VARCHAR(25)
+);
+
+INSERT INTO doctors VALUES('', 'Ona', 'Onute');
+INSERT INTO doctors VALUES('', 'Lita', 'Onota');
+INSERT INTO doctors VALUES('', 'Koris', 'Pita');
+
+
+UPDATE doctors
+    SET name = 'Joe', lname = 'Riko'
+    WHERE id = 6;
+
+
+    CREATE TABLE patients (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(25),
+        lname VARCHAR(25),
+        doctor_id INT(6)
+    );
+
+    INSERT INTO patients VALUES('', 'Mark', 'John', '2');
+    INSERT INTO patients VALUES('', 'Tom', 'Karlos', '2');
+    INSERT INTO patients VALUES('', 'Aris', 'Tido', '1');
+    INSERT INTO patients VALUES('', 'Tom', 'Karlos', '3');
+    INSERT INTO patients VALUES('', 'Co', 'John', '1');
+    INSERT INTO patients VALUES('', 'Tom', 'Nano', '5');
+    INSERT INTO patients VALUES('', 'Jili', 'Jato', '4');
+
+
+    CREATE TABLE img (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        patients_id INT(6)
+    );
+
+
+    INSERT INTO img VALUES('', 'foto.jpg', '3');
+    INSERT INTO img VALUES('', '3.png', '1');
+    INSERT INTO img VALUES('', '1.jpg', '1');
+    INSERT INTO img VALUES('', '3.gif', '2');
+    INSERT INTO img VALUES('', '0.jpg', '4');
+
+
+
+
+
+    DELETE FROM  doctors
+    WHERE id = 7;
 
 SELECT USER, PASSWORD, password_expired FROM user;
 SELECT * FROM user;
