e7a8ec2b51b02e479fe74196967747a1d89561da
Gustas <augustasbu@gmail.com>
2017-12-05 13:55:52 +0200

    komit po pietu

--- uzduotys/MAMP/uzduotis17/gustas/1-mysql-hospital11/index.php
+++ uzduotys/MAMP/uzduotis17/gustas/1-mysql-hospital11/index.php
@@ -8,7 +8,7 @@ define( "DB_NAME", 'hospital11');
 $connection = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if($connection){
-    echo "sekmingai prisijungete <br />";
+    echo "sekmingai prisijungete <br /> <br />";
 } else {
     die ("prisijungimo klaida <br />" . mysqli_connect_error());
 }
@@ -16,10 +16,8 @@ if($connection){
 
 
 
-
-
 function getDoctor($prisijung, $nr){ //nr ir prisijung yra local!!!!!!!!
-    $mano_sql = "select*FROM doctors
+    $mano_sql = "SELECT * FROM doctors
                         WHERE id=$nr;
                 ";
                 //ivykdo virsuj apsirasyta sql funkcija
@@ -38,8 +36,10 @@ print_r($daktaras);
 
 echo "<br />Daktaras:" . $daktaras['name'] . "<br />";
 
+// uzduotis 2
+// sukurti f-ja createDoctor(...)
 function createDoctor($connection, $vardas, $pavarde){
-    $mano_sql = "INSERT INTO doctors VALUES('', '$vardas', '$pavarde');";
+    $mano_sql = "INSERT INTO doctors VALUES('17', '$vardas', '$pavarde');";
     $uzklausa = mysqli_query($connection, $mano_sql);
     if($uzklausa){
         echo"issaugojom daktara <br />";
@@ -49,11 +49,55 @@ function createDoctor($connection, $vardas, $pavarde){
 }
 
 
-createDoctor($connection, "Pavlovas", "Klepas");
-$daktaras = getDoctor($connection, 10);
-print_r($daktaras);
+// createDoctor($connection, "Secilija", "Po");
+// $daktaras = getDoctor($connection, 10);
+// print_r($daktaras);
 
 
+// uzduotis 3
+// sukurti f-ja deleteDoctor($id)
+
+function deleteDoctor($connection, $kelintas){
+    $mano_sql = "DELETE FROM doctors
+                WHERE id = '$kelintas';";
+    $uzklausa = mysqli_query($connection, $mano_sql);
+    if($uzklausa){
+        $mano_sql = "SELECT * FROM doctors
+                    WHERE id = '$kelintas'";
+        mysqli_query($connection, $mano_sql);
+        echo "<br />daktaras istrintas <br />";
+    } else {
+        echo 'KLAIDA: nepavyko istrinti gydytojo. ' . mysqli_error($connection);
+    }
+}
+
+// deleteDoctor($connection, 14);
+
+
+// uzduotis 4
+// sukurti f-ja editDoctor(...);
+function editDoctor($connection, $kelintas, $vardas, $pavarde){
+    $mano_sql = "UPDATE doctors SET
+        name = '$vardas',
+        lname = '$pavarde'
+         WHERE id=$kelintas;";
+    $uzklausa = mysqli_query($connection, $mano_sql);
+
+    if($uzklausa){
+        echo"Duomenys pakeisti<br />";
+    } else{
+        echo 'KLAIDA: nepavyko pakeisti gydytojo. ' . mysqli_error($connection);
+    }
+}
+
+$keitimas = editDoctor($connection, 15, 'Ponas', 'Lopas');
+print_r($keitimas);
+
+
+// uzduotis 5
+// sukurti f-ja getDoctors();
+
+//
 
 
 
