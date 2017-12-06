463344d242121bfe181ddd1591afec10831baf1e
grajauskas <e.grajauskas@gmail.com>
2017-12-06 10:38:02 +0200

    18d

---
+++ uzduotys/MAMP/uzduotis18/destytojas/1-php-abstract-class/moteris.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Moteris extends Zmogus{
+    private $estrogenai = 1000;
+    // overwriting tevo f-ja
+    function print(){
+        echo "Moteris<br>";
+    }
+    // fucntion 
+}
