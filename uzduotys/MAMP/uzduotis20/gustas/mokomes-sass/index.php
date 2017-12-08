220205947f84740a04c3aa3b46f53ed36d43c611
Gustas <augustasbu@gmail.com>
2017-12-08 11:53:25 +0200

    dar vienas

---
+++ uzduotys/MAMP/uzduotis20/gustas/mokomes-sass/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
+        <!-- !!! mano CSS visada zemiau -->
+        <link rel="stylesheet" href="./css/main.css?ver=1.0.0">
+        <link rel="stylesheet" href="./css/scss-dist.css?ver=1.0.0">
+
+    </head>
+    <body  >
+
+        <div class="container">
+            <h1>Isijunk konsole</h1>
+
+            <section>
+                <div id='antraste'>
+                    Antraste
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+
+            <aside>
+                <div id='antraste2'>
+                    Antra antraste
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </aside>
+
+        </div>
+
+
+        <!--  !!! patartina failo apacioje   -->
+
+        <script type="text/javascript" src="./libs/jquery-3.2.1.js"> </script>
+        <!-- !!! mano js failas visada zemiau -->
+        <script type="text/javascript" src="./main.js?ver=1.0.0"> </script>
+
+    </body>
+</html>
