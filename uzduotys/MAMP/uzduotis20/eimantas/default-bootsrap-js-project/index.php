6070d501f4e022445a5fa29f6f4dc00d76a2d93b
KCSeimantas <eimutisuk@live.com>
2017-12-11 14:21:47 +0200

    wip

---
+++ uzduotys/MAMP/uzduotis20/eimantas/default-bootsrap-js-project/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
+        <!-- !!! mano CSS visada zemiau -->
+        <link rel="stylesheet" href="./css/main.css?ver=1.0.0">
+
+    </head>
+    <body>
+
+    <div class="row">
+        <div class="col-8">
+            <section>
+                <article class="antraste">
+
+                </article>
+            </section>
+            <section>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </section>
+        </div>
+        <div class="col-4">
+            <div class="row">
+                <aside class="">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </aside>
+            </div>
+        </div>
+    </div>
+
+
+
+
+        <!--  !!! patartina failo apacioje   -->
+
+        <script type="text/javascript" src="./libs/jquery-3.2.1.js"> </script>
+        <!-- !!! mano js failas visada zemiau -->
+        <script type="text/javascript" src="./main.js?ver=1.0.0"> </script>
+
+    </body>
+</html>
